9e3cabde6a6ed5d24abdf487c732bef93a955cc8
FAHIM-7HEVEN <www.fahimahmedraj07@gmail.com>
2026-05-08 15:32:08 +0600

    Update kiss.js

--- Script/commands/kiss.js
+++ Script/commands/kiss.js
@@ -90,7 +90,7 @@ module.exports.run = async function ({ event, api }) {
 
   } catch {
     return api.sendMessage(
-      "API Error Call Boss SAHU",
+      "API Error Call Boss 𝐅𝐀𝐇𝐈𝐌",
       threadID,
       messageID
     );
